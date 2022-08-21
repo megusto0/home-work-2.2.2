28358f1841c48bd8ad16630a409368874abf17e0
megusto0 <ruslan00900@gmail.com>
2022-08-21 13:07:48 +0400

    hw

---
+++ src/ServiceStation.java
@@ -0,0 +1,10 @@
+public class ServiceStation {
+    public void check(Vehicle vehicle) {
+            System.out.println("Обслуживаем " + vehicle.getModelName());
+            for (int i = 0; i < vehicle.getWheelsCount(); i++) {
+                vehicle.updateTyre();
+            }
+            vehicle.checkEngine();
+            vehicle.checkTrailer();
+    }
+}
